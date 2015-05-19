721fe1b58da005e0a2955647caf99908f3af9747
darren lee <darrenl@10cms.com>
2015-05-19 17:55:23 +0100

    URL construction for template and asset url no longer uses path.join because this is os specific and these helpers always return a URL which shoudl use a /.

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ path = require('path'),
 rename = require('gulp-rename'),
 
 through = require('through2');
+url = require('url');
 
 var DEFAULT_S3_PREFIX = exports.DEFAULT_S3_PREFIX = '';
 var DEFAULT_TASK_PREFIX = exports.DEFAULT_TASK_PREFIX = 'condensation';
@@ -82,7 +83,7 @@ Condensation.prototype.condense = function() {
     });
 
     templateData.s3 = s3opts.aws;
-    templateData.s3.awsPath = s3.endpoint.href+path.join(s3opts.aws.bucket,s3opts.prefix);
+    templateData.s3.awsPath = s3.endpoint.href+url.resolve(s3opts.aws.bucket,s3opts.prefix);
 
     gulp.task(self.genTaskName('build',i),[self.genTaskName('clean:errors')],function() {
 
