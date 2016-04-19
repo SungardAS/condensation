87c4aed74bf729bc45f687e4595627e6f0b0aa95
Kevin McGrath <kevin.mcgrath@sungardas.com>
2016-04-19 01:06:59 -0400

    test for invalid templates

--- test/integration/invalid.test.js
+++ test/integration/invalid.test.js
@@ -1,56 +1,65 @@
 var _ = require('lodash'),
-async = require('async'),
-clone = require('clone'),
-assert = require('assert'),
-path = require('path'),
-shared = require('./project-shared'),
-fs = require('fs');
+  async = require('async'),
+  clone = require('clone'),
+  assert = require('assert'),
+  path = require('path'),
+  shared = require('./project-shared'),
+  fs = require('fs');
 
 var projectDir = 'test/fixtures/projects/invalid';
 var distDir = 'test/dist/invalid';
 
-var tasks = [
-  'build',
-  'build:0',
-  'clean',
-  'default',
-  'deploy',
-  'deploy:0',
-  's3:bucket:ensure:0',
-  's3:list',
-  's3:objects:write:0'
-];
+var projectConfig = {
+  s3: [
+    {
+      aws: {
+        region: 'us-east-1',
+        bucket: '',
+      },
+      validate: false,
+      create: false
+    }
+  ],
+  projectName: 'invalid',
+  root: projectDir,
+  taskPrefix: '',
+  dist: distDir,
+};
 
-var distributionFiles = [
-  'particles/cftemplates/vpc.template',
-  'particles/cftemplates/subnet.template',
-  'particles/cftemplates/infra.template'
-];
 
-
-describe.only('invalid', function(){
+describe('invalid', function(){
   var gulp;
 
-  shared.shouldBehaveLikeAProject({
-    gulp: gulp,
-    tasks: tasks,
-    distributionFiles: distributionFiles,
-    projectConfig: {
-        s3: [
-          {
-            aws: {
-              region: 'us-east-1',
-              bucket: '',
-            },
-            validate: false,
-            create: false
-          }
-        ],
-        projectName: 'invalid',
-        root: projectDir,
-        taskPrefix: '',
-        dist: distDir,
-      }
+  beforeEach('create new gulp object', function() {
+    gulp = clone(require('gulp'));
+    require('../../').buildTasks(
+      gulp,
+      projectConfig
+    );
+  });
+
+  after('clean the project', function(done) {
+    var afterGulp = clone(require('gulp'));
+    require('../../').buildTasks(afterGulp,projectConfig);
+    afterGulp.start('clean');
+    afterGulp.on('stop',function(){
+      fs.lstat(projectConfig.dist, function(err, stats) {
+        assert(err,projectConfig.dist + " not clean");
+        done();
+      });
+    });
+  });
+
+  it('should fail to build the project', function(done){
+    gulp.start('build');
+
+    gulp.on('err', function(err) {
+      assert(err);
+    });
+    gulp.on('stop', function() {
+      done();
+    });
+
   });
 
 });
